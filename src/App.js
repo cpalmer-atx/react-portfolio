ca9aaa446d1feb90b719a1cc101accd900508e90
Chad Palmer <chad.n.palmer@gmail.com>
2021-05-04 15:39:03 -0500

    navbar style completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
