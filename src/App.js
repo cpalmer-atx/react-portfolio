673635693cdbc12c3bfb6d48b2f8813e768b79f1
Chad Palmer <chad.n.palmer@gmail.com>
2021-05-04 16:54:24 -0500

    nav sidebar finished without transitions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './components/sidebar';
 
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
