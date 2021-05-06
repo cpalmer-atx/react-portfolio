765bdde7614b5db9606614808a2e8a429cb3e6ad
Chad Palmer <chad.n.palmer@gmail.com>
2021-05-06 12:35:59 -0500

    small code cleanup after img issue resolution

--- src/components/infoSection/Data.js
+++ src/components/infoSection/Data.js
@@ -1,4 +1,4 @@
-module.exports.homeObjOne = {
+export const homeObjOne = {
   id: 'about',
   lightBg: true,
   lightText: false,
@@ -10,12 +10,7 @@ module.exports.homeObjOne = {
   buttonLabel: 'Get started',
   imgStart: false,
   img: require('../../images/svg-2.svg').default,
-  // img: '../../images/Nikola-Tesla-2-by-Merlin2525.svg',
   alt: 'Car',
   dark: true,
   primary: true,
 };
-
-// module.exports homeObjOne
-
-// BUG: img does not render, currently importing directly in index.js as temporary fix
