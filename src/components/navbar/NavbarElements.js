08cbebc8c462712e2009d040dbb7d34ccbbe3520
Chad Palmer <chad.n.palmer@gmail.com>
2021-05-11 18:40:46 -0500

    minor navbar troubleshooting

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -5,7 +5,7 @@ import { Link as LinkS } from 'react-scroll';
 export const Nav = styled.nav`
   background: #000;
   height: 80px;
-  margin-top: -80px;
+  // margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -81,7 +81,7 @@ export const NavLinks = styled(LinkS)`
   height: 100%;
   cursor: pointer;
 
-  &.active {
+  &:active {
     border-bottom: 3px solid #01bf71;
   }
 
