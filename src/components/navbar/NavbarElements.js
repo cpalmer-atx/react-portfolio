1ab0537e86e1312b381d9a731ff7cfc447f6043a
Chad Palmer <chad.n.palmer@gmail.com>
2021-05-12 17:39:20 -0500

    styled all navbar components

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -1,21 +1,121 @@
 import styled from 'styled-components';
 import { Link as LinkRouter } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
+import Colors from '../../colors.js';
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  background: ${Colors.PRIMARY_BG};
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
 
-export const NavbarContainer = styled.div``;
+  @media screen and (max-width: 960px) {
+    transition: 0.4s all ease;
+  }
+`;
 
-export const NavLogo = styled(LinkRouter)``;
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1100px;
+`;
 
-export const MobileIcon = styled.div``;
+export const NavLogo = styled(LinkRouter)`
+  color: ${Colors.WARM_ACCENT};
+  justify-self: flex-start;
+  cursor: pointer;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+  font-weight: bold;
+  text-decoration: none;
+`;
 
-export const NavItems = styled.ul``;
+export const MobileIcon = styled.div`
+  display: none;
 
-export const NavItem = styled.li``;
+  @media screen and (max-width: 768px) {
+    color: ${Colors.WARM_ACCENT};
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
 
-export const NavLinks = styled(LinkScroll)``;
+export const NavItems = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  margin-right: -22px;
 
-export const ResumeBtn = styled.nav``;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
-export const ResumeBtnLink = styled(LinkRouter)``;
+export const NavItem = styled.li`
+  height: 80px;
+`;
+
+export const NavLinks = styled(LinkScroll)`
+  color: ${Colors.SECONDARY_BG};
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  
+  &:active {
+    border-bottom: 7px solid ${Colors.WARM_ACCENT};
+  }
+
+  &:hover {
+    transition: all 0.1s ease-in-out;
+    color: ${Colors.WHITE};
+  }
+`;
+
+export const ResumeBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const ResumeBtnLink = styled(LinkRouter)`
+  background: ${Colors.WARM_ACCENT};
+  white-space: nowrap;
+  padding: 10px 22px;
+  color: ${Colors.SECONDARY_BG};
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  text-decoration: none;
+  border-radius: 20px;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: ${Colors.COOL_ACCENT};
+    color: ${Colors.WHITE};
+  }
+`;
