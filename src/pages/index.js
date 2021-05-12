fd30a549c0f8c5de82186be50f85aa02adb5a22e
Chad Palmer <chad.n.palmer@gmail.com>
2021-05-12 14:45:59 -0500

    set up app/home

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Navbar from '../components/navbar';
+import NavMenu from '../components/navMenu';
+
+const Home = () => {
+  const [open, setOpen] = useState(false);
+  const toggle = () => { setOpen(!open); }
+
+  return (
+    <>
+      <NavMenu open={open} toggle={toggle} />
+      <Navbar toggle={toggle} />
+    </>
+  )
+}
+
+export default Home;
